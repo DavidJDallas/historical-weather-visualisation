8302a19d7063a45830c97e855d767845841443e8
David Dallas <Daviddallas606@gmail.com>
2023-11-01 14:10:45 +0000

    rain files started

--- src/Components/Dashboard/Rain/IndexRain.tsx
+++ src/Components/Dashboard/Rain/IndexRain.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { IndexProps } from './RainTypes';
+import RainByMonth from './RainByMonth';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Rain = ({dataBySeason, dataByMonth, dataByYear, formattedData}: IndexProps): JSX.Element => {
 
     return(<>
     <h1>Rain</h1>
+    <Container>
+        <RainByMonth/>
+
+    </Container>
 
     
 
---
+++ src/Components/Dashboard/Rain/RainByMonth.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+
+const RainByMonth = () => {
+
+    return(<>
+    </>)
+
+}
+
+export default RainByMonth
