135ed088f14d10aff0ceb646fad13fec5b88dd13
David Dallas <Daviddallas606@gmail.com>
2023-11-04 11:19:30 +0000

    d3 notes added in

---
+++ src/d3Notes.js
@@ -0,0 +1,27 @@
+//At its core, D3 is a lib that allows users to draw low-level, highly customisable interactive graphs using JS. It uses SVGs and works by manipulating SVGs using various functions. 
+
+//Selections
+
+//To make selections, use either d3.select() or d3.selectAll(). The former chooses the first one it finds, and the latter selects all. Both functions take a string as their only arguments. 
+
+//Once elements are selected, they can be modified using:
+//.style()
+//.attr()
+// .classed()
+// .property()
+// .text()
+// .html()
+
+//Event Handlers
+
+//Event handlers can be added to selected methods using the .on() method.
+//Some common ones:
+// .click()
+// .mouseenter()
+// .mouseover()
+// .mouseleave()
+// .mouseout()
+// .mousemove()
+
+//Inserting and removing elements
+
