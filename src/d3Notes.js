832553d3f99991663184b0fb4f250f4a2646ce7c
David Dallas <Daviddallas606@gmail.com>
2023-11-06 09:00:09 +0000

    more notes added

--- src/d3Notes.js
+++ src/d3Notes.js
@@ -4,6 +4,8 @@
 
 //To make selections, use either d3.select() or d3.selectAll(). The former chooses the first one it finds, and the latter selects all. Both functions take a string as their only arguments. 
 
+//Both select and selectAll return an array of HTML nodes.
+
 //Once elements are selected, they can be modified using:
 //.style()
 //.attr()
@@ -31,3 +33,14 @@
 //The .each() method lets you call a function for each element of a section. 
 //The .call() method allows a function to be called into which the selection itself is passed as the first argument. Each() iterates over each element, and .call() invokes a function once and passes the entire selection to that function.
 //Filter() can also be used, in the same way that you would use it for JS.
+
+
+//Data Joins
+
+//D3 focuses on a data-driven approach. To make D3 aware of the data, the data() metod is used on a selection of DOM elements to attach the data to those elements. The data set is passed as an argument to the method. 
+
+//If you have more data than elements, use enter(). If more elements than data, use exit().
+
+//Styling
+
+//Using a lot of inline styles on HTML elements can be hard to manage, and is often easier to use class elements. 
