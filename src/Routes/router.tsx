6ef1be17554b61deb1d4084e44cc84979d325cbd
David Dallas <Daviddallas606@gmail.com>
2023-11-13 09:16:27 +0000

    more wind links removed

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -2,12 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Route, Routes,createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 import App from '../App';
-import MainDashboard from '../Components/Dashboard/Index';
-import Rain from '../Components/Dashboard/Rain/IndexRain';
-import Temperature from '../Components/Dashboard/Temperature/IndexTemperature';
-import Wind from '../Components/Dashboard/Wind/IndexWind';
-import ErrorScreen from '../Components/ErrorScreen';
-import LandingPage from '../Components/LandingPage/Index';
+
 
 
 const router = createBrowserRouter(
