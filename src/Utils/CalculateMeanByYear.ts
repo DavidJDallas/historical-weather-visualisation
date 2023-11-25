86c6f7e33a8278eb104027922f46d6c6b1dcb4a1
David Dallas <Daviddallas606@gmail.com>
2023-11-25 18:36:18 +0000

    types updated for CalculateMeanByYear Utils function

--- src/Utils/CalculateMeanByYear.ts
+++ src/Utils/CalculateMeanByYear.ts
@@ -1,16 +1,16 @@
 import * as d3 from 'd3';
-import { GroupedDataByYear } from '../Components/Dashboard/Types';
+import { FormattedData, GroupedDataByYear } from '../Components/Dashboard/Types';
 
 export const calculateMeanByYear = (dataByYear: GroupedDataByYear[], yearValue: number, variable: string) => {
     
-    const filteredDataByYear = dataByYear.filter((year: any) => year.year >= yearValue);
+    const filteredDataByYear = dataByYear.filter((year: GroupedDataByYear) => year.year >= yearValue);
     return filteredDataByYear.map((year) => ({
 
         year: year.year,
         [variable]: variable === 'temperature' ? 
-        (d3.mean(year.data.map((element: any) => Number(element.temperatureMax))) as number) ?? 0 
+        (d3.mean(year.data.map((element: FormattedData) => Number(element.temperatureMax))) as number) ?? 0 
         : 
-        (d3.mean(year.data.map((element) => Number(element.rain)))as number)
+        (d3.mean(year.data.map((element: FormattedData) => Number(element.rain)))as number)
         ?? 0
 
     }))
