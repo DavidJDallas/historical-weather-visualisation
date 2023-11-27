86540665d04c3f54562d1ba31d94b955c2bbe5ae
David <110460234+DavidCobb606@users.noreply.github.com>
2023-11-27 16:30:42 +0000

    bar chart template added into tempbyseason graph

--- src/Components/Dashboard/Temperature/TempBySeason.tsx
+++ src/Components/Dashboard/Temperature/TempBySeason.tsx
@@ -8,9 +8,7 @@ import BarChartTemplate from '../../../Graphs/BarChart';
 
 const TempBySeason = ({dataBySeason, width, height, yearValue}: TempBySeasonProps): JSX.Element => {
 
-    const [tempData, setTempData] = useState<TempDataSeason[] | []>([])
-    const chartRef = useRef<SVGSVGElement | null>(null);
-
+    const [tempData, setTempData] = useState<TempDataSeason[] | []>([]);
 
     useEffect(() => {
         const filteredDataBySeason: FilteredDataBySeason[] = filterDataByYear(dataBySeason, yearValue);
@@ -18,104 +16,9 @@ const TempBySeason = ({dataBySeason, width, height, yearValue}: TempBySeasonProp
        const calculateMean: TempDataSeason[] = filteredDataBySeason.map((object) => ({
                season: object.season,
                temperature: d3.mean((object.data.map((element: FormattedData) => element.temperatureMax)))
-           }))
+           }));
        setTempData(calculateMean);
    }, [dataBySeason, yearValue]);
-
-
-   useEffect((): void => {                 
-
-    if(tempData.length>0){  
-
-        d3.select(chartRef.current).selectAll('*').remove();
-        
-        let adjustedWidth = width-30
-        
-        const xScale = d3.scaleLinear()
-                            .domain([0, tempData.length])
-                            .range([30, adjustedWidth]);
-
-        const yScale = d3.scaleLinear()
-                        .domain([d3.min(tempData.map((element) => element.temperature /1.2) as number[]) ?? 0, d3.max(tempData.map((element) => element.temperature *1.2) as number[]) ?? 0])
-                            .range([height, 50]);
-
-        const xAxis = d3.scaleBand()
-                            .domain(tempData.map((x) => x.season))
-                            .range([30, adjustedWidth])
-                            .padding(0);
-
-        const yAxis = d3.axisLeft(yScale)
-                        .tickFormat(d => d.toString().slice(0,5))
-
-        const colourScale = d3.scaleSequential()
-                        .domain([0, d3.max(tempData.map((element) => element.temperature))])
-
-        const interpolatorColourFunction = d3.interpolateRgb('blue', 'red')
-
-
-        const svg= d3.select(chartRef.current)
-                            .append('svg')
-                            .attr('width', width)
-                            .attr('height', height+200);
-
-        d3.selectAll('.tempMonthToolTip').remove();
-        
-        const tooltip = d3.select('body').append('div')
-                            .style('position', 'absolute')
-                            .style('z-index', '10')
-                            .style('visibility', 'hidden')
-                            .style('background-color', 'white')
-                            .style('border-style', 'solid')
-                            .style('border-width', '2px')
-                            .style('border-color', '#50e991')
-                            .style('padding', '5px')
-                            .style('font-size', '12px');
-
-        svg.selectAll('rect')
-                .data(tempData)
-                .enter()
-                .append('rect')
-                .attr('x', (d,i) => xScale(i))
-                .attr('y', d => yScale(d.temperature))
-                .attr('width', xScale(5)-xScale(4) -20)
-                .attr('height', (d, i) => (height-yScale(d.temperature) ))
-                .attr('fill', d=> interpolatorColourFunction(colourScale(d.temperature)))                        
-                .on('mouseover', (event, d) => {
-                        tooltip.html(`${(d.season)}: ${String(d.temperature).slice(0,6)} `+ '&deg c')
-                            .style('visibility', 'visible')
-                    })
-                .on('mousemove', (event) => {
-                        tooltip.style('top', event.pageY - 10 + 'px')
-                        tooltip.style('left', event.pageX + 10 + 'px')
-                    })
-                .on('mouseout', () => {
-                        tooltip.style('visibility', 'hidden')
-                    });
-
-                
-         svg.append('text')
-               .attr('x', width/2)
-               .attr('y', 30)
-               .style('text-anchor', 'middle')
-               .style('font-size', '18px')
-               .text(`Average temperature by Season ()`);
-
-        svg.append('g')
-                .attr('transform', `translate(0, ${height})`)                
-                .call(d3.axisBottom(xAxis))
-                .selectAll('text')
-                .style('font-size', '13px')
-        
-        svg.append('g')
-            .attr('transform', 'translate(30,0)')
-            .call(yAxis)
-            .selectAll('text')
-            .style('font-size', '9px')
-
-    }
-
-}, [tempData, height, width]);
-
 
     return(
         <>
@@ -124,21 +27,18 @@ const TempBySeason = ({dataBySeason, width, height, yearValue}: TempBySeasonProp
               width={width}
               height={height}
               yearValue={yearValue}
-              title={'Average Rainfall by Day Per Month(mm)'}
-              interpolateFirstColour={'#77ccff'}
-              interpolateSecondColour={'#0066ff'}
+              title={'Average Temperature Per Month (mm)'}
+              interpolateFirstColour={'blue'}
+              interpolateSecondColour={'red'}
               xAccessor={'season'}
               yAccessor={'temperature'}
               domainFirstValueX={0}
               domainFirstValueY={0}
-              sliceLength={3}
-              gapBetweenBars={2}
-        
+              sliceLength={8}
+              gapBetweenBars={3}
         />
-               <svg className=''ref={chartRef} height={'100%'} width={'100%'} preserveAspectRatio='xMinYMin meet' ></svg>
         </>
     )
-
 }
 
-export default TempBySeason
+export default TempBySeason;
