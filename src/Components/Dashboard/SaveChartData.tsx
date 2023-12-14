b523c1eb19804022195c56bcd864d9f1cb137568
David Dallas <Daviddallas606@gmail.com>
2023-12-14 20:35:12 +0000

    note added in

--- src/Components/Dashboard/SaveChartData.tsx
+++ src/Components/Dashboard/SaveChartData.tsx
@@ -15,6 +15,8 @@ const SaveChartData = () => {
     const searchContext: SearchContextProps = useContext<SearchContextProps>(SearchContext);
     const {place, postcode, weatherData} = searchContext;
 
+    //Next: Work out how I'm going to reference the stored data in future calls. How will I know what I'm calling on the backend DB? Maybe just a request for get all and have it on the backend that data is temporary and gets wiped after every session? And create an endpoint on backend for users to delete all data or delete last data, in which case the last data point would just easily be accessed via the only local storage?
+    
     const handleClick = () => {
         postHistoricalWeatherData(weatherData)
     }
