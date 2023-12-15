f73273753e5d11989a962558890e37447ad9138a
David <110460234+DavidCobb606@users.noreply.github.com>
2023-12-15 21:34:56 +0000

    minor formatting change on savechartdata

--- src/Components/Dashboard/SaveChartData.tsx
+++ src/Components/Dashboard/SaveChartData.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
 import { SaveChartDataProps } from './Types';
-import { getGeoLocationData } from '../../Utils/getGeoLocationData';
-import { LatAndLong } from '../../Services/ServicesTypes';
 import { useContext } from 'react';
 import { SearchContext } from '../../Context/SearchContext';
 import { SearchContextProps } from '../../Context/ContextTypes';
 import { postHistoricalWeatherData } from '../../Services/APICalls';
-
-
 
 const SaveChartData = () => {
 
