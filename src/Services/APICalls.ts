e238935e00fbc4d9c4c11db4205d07c81bcccbf9
David Dallas <Daviddallas606@gmail.com>
2023-12-17 20:11:04 +0000

    api getbyID call added into API calls

--- src/Services/APICalls.ts
+++ src/Services/APICalls.ts
@@ -96,6 +96,18 @@ export const getAllWeatherDataFromBackend = async(): Promise<DailyData | unknown
     }
 }
 
+export const getWeatherDataById = async(id: string) => {
+    try{
+        const response = await axios.get(`http://localhost:3000/api/main${id}`)
+    } catch(error: unknown){
+        if(error instanceof AxiosError){               
+            return `There was an error with the API call: ${error}`
+        } else {
+            return `Non-axios related error: ${error}`
+        }
+    }
+}
+
 export const deleteWeatherDataFromBackend = async(): Promise<string | unknown | AxiosError> => {
     try{
          const response = await axios.delete(`http://localhost:3000/api/main`)
